4bdbe59b62b10016ec867ddee2e3bc3494f63afc
jhonnierandrey <jhonnierandrey@outlook.com>
2020-11-24 19:03:53 -0300

    Link to GitHub Repo added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
         return (
             <footer id="footer">
 				<p>© 2020 JAES Made It</p>
-                <p>Made with <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a></p>
+                <p>Made with <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a> | <a href="https://github.com/jhonnierandrey/covid-board" target="_blank" rel="noopener noreferrer">GitHub</a></p>
                 <p><a href="http://jhonnierandrey.info/" target="_blank" rel="noopener noreferrer">See more</a></p>
 			</footer>
         );
