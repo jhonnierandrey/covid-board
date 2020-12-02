436d03f0b1b03513c54151d0c6ce9ab4da62a24a
jhonnierandrey <jhonnierandrey@outlook.com>
2020-12-02 19:33:32 -0300

    New object with LATAM countries added.

---
+++ src/components/countries.js
@@ -0,0 +1,75 @@
+const countries = [{
+    id: "AR",
+    name: "Argentina"
+},{
+    id: "BO",
+    name: "Bolivia"
+},{
+    id: "BR",
+    name: "Brasil"
+},{
+    id: "CL",
+    name: "Chile"
+},{
+    id: "CO",
+    name: "Colombia"
+},{
+    id: "CR",
+    name: "Costa Rica",
+},
+{
+    id: "CU",
+    name: "Cuba",
+},
+{
+    id: "EC",
+    name: "Ecuador",
+},
+{
+    id: "SV",
+    name: "El Salvador",
+},
+{
+    id: "GT",
+    name: "Guatemala",
+},
+{
+    id: "HT",
+    name: "Haití",
+},
+{
+    id: "HN",
+    name: "Honduras",
+},
+{
+    id: "MX",
+    name: "México",
+},
+{
+    id: "NI",
+    name: "Nicaragua",
+},
+{
+    id: "PA",
+    name: "Panamá",
+},
+{
+    id: "PY",
+    name: "Paraguay",
+},
+{
+    id: "PE",
+    name: "Perú",
+},{
+    id: "DO",
+    name: "República Dominicana",
+},{
+    id: "UY",
+    name: "Uruguay",
+},{
+    id: "VE",
+    name: "Venezuela",
+}
+]
+
+export default countries;
