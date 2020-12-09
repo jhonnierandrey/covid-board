66832453711c1844af98f53f7bd35a6eed923dd9
jhonnierandrey <jhonnierandrey@outlook.com>
2020-12-09 20:57:29 -0300

    Minor improvements to Title element

--- src/components/Title.js
+++ src/components/Title.js
@@ -6,6 +6,22 @@ function Title(props) {
     let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     let today  = new Date();
 
+    function showChart() {
+        let viewContainer = document.querySelector(".chart-view-container");
+        let showChartBtn = document.querySelector('.show-chart-btn').querySelector('button').querySelector('span');
+
+        console.log(showChartBtn);
+
+        //viewContainer.style.display = "block";
+        if (viewContainer.style.display === "none") {
+            viewContainer.style.display = "block";
+            showChartBtn.innerText = "Ocultar tabla";
+        } else {
+            viewContainer.style.display = "none";
+            showChartBtn.innerText = "Mostrar tabla";
+        }
+    }
+
     return(
         <div className="title jumbotron">
             <div className="card-container col">
@@ -33,6 +49,11 @@ function Title(props) {
                 />
             </div>
             <hr className="my-4" />
+            <div className="show-chart-btn">
+                <button onClick={showChart} type="button" class="btn btn-warning">
+                    <span>Mostrar tabla</span> <i class="far fa-chart-bar"></i>
+                </button>
+            </div>
         </div>
         );
     }
