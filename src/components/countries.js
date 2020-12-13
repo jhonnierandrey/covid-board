811051ccd91422cb14f25b4343e222624d4ba462
jhonnierandrey <jhonnierandrey@outlook.com>
2020-12-13 00:39:45 -0300

    new key and value for countries = name_en

--- src/components/countries.js
+++ src/components/countries.js
@@ -1,74 +1,98 @@
 const countries = [{
+    id: null,
+    name: "Global",
+    name_en : "Global",
+},{
     id: "ar",
-    name: "Argentina"
+    name: "Argentina",
+    name_en : "Argentina",
 },{
     id: "bo",
-    name: "Bolivia"
+    name: "Bolivia",
+    name_en : "Bolivia"
 },{
     id: "br",
-    name: "Brasil"
+    name: "Brasil",
+    name_en : "Brazil"
 },{
     id: "cl",
-    name: "Chile"
+    name: "Chile",
+    name_en : "Chile"
 },{
     id: "co",
-    name: "Colombia"
+    name: "Colombia",
+    name_en : "Colombia"
 },{
     id: "cr",
     name: "Costa Rica",
+    name_en : "Costa Rica"
 },
 {
     id: "cu",
     name: "Cuba",
+    name_en : "Cuba"
 },
 {
     id: "ec",
     name: "Ecuador",
+    name_en : "Ecuador"
 },
 {
     id: "sv",
     name: "El Salvador",
+    name_en : "El Salvador"
 },
 {
     id: "gt",
     name: "Guatemala",
+    name_en : "Guatemala"
 },
 {
     id: "ht",
     name: "Haití",
+    name_en : "Haiti"
 },
 {
     id: "hn",
     name: "Honduras",
+    name_en : "Honduras"
 },
 {
     id: "mx",
     name: "México",
+    name_en : "Mexico"
 },
 {
     id: "ni",
     name: "Nicaragua",
+    name_en : "Nicaragua"
 },
 {
     id: "pa",
     name: "Panamá",
+    name_en : "Panama"
 },
 {
     id: "py",
     name: "Paraguay",
+    name_en : "Paraguay"
 },
 {
     id: "pe",
     name: "Perú",
+    name_en : "Peru"
 },{
     id: "do",
     name: "República Dominicana",
+    name_en : "Dominican Republic"
 },{
     id: "uy",
     name: "Uruguay",
+    name_en : "Uruguay"
 },{
     id: "ve",
     name: "Venezuela",
+    name_en : "Venezuela"
 }
 ]
 
