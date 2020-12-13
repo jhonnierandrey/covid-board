0f062783648fb1040f01a145d326d14026513640
jhonnierandrey <jhonnierandrey@outlook.com>
2020-12-13 19:00:49 -0300

    Navbar list countries quickly

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,47 +24,6 @@ function Navbar(props) {
         callApi()
     }
 
-    function createCountryList(){
-        let countryList = document.querySelector(".dropdown-menu");
-
-        if (countryList.querySelector(".dropdown-item")) {
-            return;
-        } else {
-            for(let i = 0; i < countries.length; i++ ){
-                countryList.innerHTML += `<a className="dropdown-item" href="/${countries[i].name}" rel="${countries[i].name}"><img src="https://flagcdn.com/w40/${countries[i].id}.png" alt="${countries[i].name}" />  ${countries[i].name}</a>`
-            }
-
-            document.querySelectorAll('.dropdown-item').forEach(item => {
-                item.addEventListener('click', e => {
-                    e.preventDefault();
-                    alert('Próximamente.')
-                })
-            })
-        }
-    }
-
-    function createCountryOptions(){
-        let countryList = document.querySelector(".country-list");
-
-        if (countryList.childElementCount > 1) {
-            return;
-        } else {
-
-            console.log('No options jet')
-            for(let i = 0; i < countries.length; i++ ){
-                countryList.innerHTML += `<option value="${i}">${countries[i].name}</option>`;
-            }
-
-        }
-    }
-
-    function changeChange(e){
-        console.log('changing country...')
-        console.log(e.target.value)
-    }
-
-    //createCountryList();
-
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <a className="navbar-brand" href="/">
@@ -76,32 +35,14 @@ function Navbar(props) {
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/Global" rel="Global" onClick={changeCountry}>Global  <i className="fas fa-globe"></i> <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/Argentina" rel="Argentina" onClick={changeCountry}>Argentina <img src="https://flagcdn.com/w40/ar.png" alt="Argentina" /></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/Colombia" rel="Colombia" onClick={changeCountry}>Colombia <img src="https://flagcdn.com/w40/co.png" alt="Colombia" /></a>
-                    </li>
-                    <li className="nav-item dropdown" onClick={createCountryList}>
-                        <a className="nav-link dropdown-toggle" href="/latam" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        LATAM
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-
-                        </div>
-                    </li>
-                    <li onClick={createCountryOptions}>
+                    <li>
                         <select className="form-control country-list" onChange={changeCountry}>
-                            <option selected hidden>LATAM</option>
+                            <option defaultValue hidden>LATAM</option>
                         </select>
                     </li>
                 </ul>
 
                 <div className="navbar-ilinks">
-                    <span>Datos de: </span>
                     <a className="btn btn-primary" href="https://covid19.who.int/" target="_blank" rel="noreferrer">WHO</a>
                     <a className="btn btn-warning" href="https://coronavirus.jhu.edu/map.html" target="_blank" rel="noreferrer">JHU</a>
                 </div>
