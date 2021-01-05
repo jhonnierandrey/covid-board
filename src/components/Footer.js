58e22f57e8f8ecd8a77a2c2e0f651b1497df65c4
jhonnierandrey <jhonnierandrey@outlook.com>
2021-01-04 23:47:33 -0300

    2021 Copyright updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
     render() {
         return (
             <footer id="footer">
-				<p>&copy; 2020 <a href="https://www.jaesmadeit.com/" target="_blank" rel="noopener noreferrer">JAES Made It</a></p>
+				<p>&copy; 2021 <a href="https://www.jaesmadeit.com/" target="_blank" rel="noopener noreferrer">JAES Made It</a></p>
                 <p>Made with <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a> | <a href="https://github.com/jhonnierandrey/covid-board" target="_blank" rel="noopener noreferrer">GitHub</a></p>
                 <p><a href="http://jhonnierandrey.info/" target="_blank" rel="noopener noreferrer">See more</a></p>
 			</footer>
